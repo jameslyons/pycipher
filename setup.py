b13a51cc39e08605521d1387c13b468eca77eada
Bradley Ayers <bradley.ayers@gmail.com>
2012-05-14 16:40:37 +1000

    Remove cruft.

--- setup.py
+++ setup.py
@@ -8,12 +8,10 @@ setup(
     description='Several simple cipher algorithms',
     author='James Lyons',
     author_email='james.lyons0@gmail.com',
-    #url='https://private.pypi.enigmainteractive.net.au.private/pypi/django-console/',
 
     packages=find_packages(exclude=['tests','tests.*']),
     include_package_data=True,  # declarations in MANIFEST.in
     test_suite='tests',
-    #install_requires=['Django>=1.2', 'django-restful>=0.1.1'],
 
     classifiers=[
         'Intended Audience :: Developers',
