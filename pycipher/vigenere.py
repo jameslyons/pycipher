b4cfc803ebf98f8a171d251eb6822c3ac99f5eaa
James Lyons <james.lyons0@gmail.com>
2013-03-29 13:02:26 +1000

    comments added, some ciphers removed, everything simplified for release

--- pycipher/vigenere.py
+++ pycipher/vigenere.py
@@ -7,10 +7,23 @@ from .base import Cipher
 
 ####################################################################################
 class Vigenere(Cipher):
+    """The Vigenere Cipher has a key consisting of a word e.g. 'FORTIFICATION'.
+        This cipher encrypts a letter according to the Vigenere tableau, the algorithm can be 
+        seen e.g. http://practicalcryptography.com/ciphers/vigenere-gronsfeld-and-autokey-cipher/
+        """
     def __init__(self,key='fortification'):
         self.key = [k.upper() for k in key]
         
     def encipher(self,string):
+        """Encipher string using Vigenere cipher according to initialised key. All punctuation and whitespace
+        is removed from the input.       
+
+        Example::
+            ciphertext = Vigenere('HELLO').encipher(plaintext)     
+
+        :param string: The string to encipher.
+        :returns: The enciphered string.
+        """           
         string = self.remove_punctuation(string)
         ret = ''
         for (i,c) in enumerate(string):
@@ -19,6 +32,15 @@ class Vigenere(Cipher):
         return ret    
 
     def decipher(self,string):
+        """Decipher string using Vigenere cipher according to initialised key. All punctuation and whitespace
+        is removed from the input.       
+
+        Example::
+            plaintext = Vigenere('HELLO').decipher(ciphertext)     
+
+        :param string: The string to decipher.
+        :returns: The enciphered string.
+        """               
         string = self.remove_punctuation(string)
         ret = ''
         for (i,c) in enumerate(string):
