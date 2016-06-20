5f3d3878f269dccd64e1a96fcecfb10760acbdf2
Andrea Di Menna <andrea.dimenna@hwlhandsets.it>
2016-06-20 14:38:28 +0200

    Fix Playfair for Python3

--- pycipher/playfair.py
+++ pycipher/playfair.py
@@ -1,10 +1,11 @@
-'''
+"""
 implements Playfair cipher
 Author: James Lyons
 Created: 2012-04-28
-'''
+"""
 from pycipher.base import Cipher
 import re
+
 
 ####################################################################################
 class Playfair(Cipher):
@@ -14,26 +15,33 @@ class Playfair(Cipher):
     
     :param key: The keysquare, as a 25 character string.
     """
-    def __init__(self,key='monarchybdefgiklpqstuvwxz'):
+    def __init__(self, key='ABCDEFGHIKLMNOPQRSTUVWXYZ'):
         self.key = [k.upper() for k in key]
         
-    def encipher_pair(self,a,b):
-        if a == b: b = 'X'
-        arow,acol = self.key.index(a)/5 , self.key.index(a)%5
-        brow,bcol = self.key.index(b)/5 , self.key.index(b)%5
-        if arow == brow: return (self.key[arow*5+(acol+1)%5] + self.key[brow*5+(bcol+1)%5])
-        elif acol == bcol: return (self.key[((arow+1)%5)*5+acol] + self.key[((brow+1)%5)*5+bcol])
-        else: return (self.key[arow*5 + bcol] + self.key[brow*5 + acol])
+    def encipher_pair(self, a, b):
+        if a == b:
+            b = 'X'
+        arow, acol = int(self.key.index(a) / 5), self.key.index(a) % 5
+        brow, bcol = int(self.key.index(b) / 5), self.key.index(b) % 5
+        if arow == brow:
+            return self.key[arow * 5 + (acol + 1) % 5] + self.key[brow * 5 + (bcol + 1) % 5]
+        elif acol == bcol:
+            return self.key[((arow + 1) % 5) * 5 + acol] + self.key[((brow + 1) % 5) * 5 + bcol]
+        else:
+            return self.key[arow * 5 + bcol] + self.key[brow * 5 + acol]
         
-    def decipher_pair(self,a,b):
-        assert a != b, 'two of the same letters occured together, illegal in playfair'
-        arow,acol = self.key.index(a)/5 , self.key.index(a)%5
-        brow,bcol = self.key.index(b)/5 , self.key.index(b)%5
-        if arow == brow: return (self.key[arow*5+(acol-1)%5] + self.key[brow*5+(bcol-1)%5])
-        elif acol == bcol: return (self.key[((arow-1)%5)*5+acol] + self.key[((brow-1)%5)*5+bcol])
-        else: return self.key[arow*5 + bcol] + self.key[brow*5 + acol]        
+    def decipher_pair(self, a, b):
+        assert a != b, 'two of the same letters occurred together, illegal in playfair'
+        arow, acol = int(self.key.index(a) / 5), self.key.index(a) % 5
+        brow, bcol = int(self.key.index(b) / 5), self.key.index(b) % 5
+        if arow == brow:
+            return self.key[arow * 5 + (acol - 1) % 5] + self.key[brow * 5 + (bcol - 1) % 5]
+        elif acol == bcol:
+            return self.key[((arow - 1) % 5) * 5 + acol] + self.key[((brow - 1) % 5) * 5 + bcol]
+        else:
+            return self.key[arow * 5 + bcol] + self.key[brow * 5 + acol]
         
-    def encipher(self,string):
+    def encipher(self, string):
         """Encipher string using Playfair cipher according to initialised key. Punctuation and whitespace
         are removed from the input. If the input plaintext is not an even number of characters, an 'X' will be appended.
 
@@ -45,14 +53,15 @@ class Playfair(Cipher):
         :returns: The enciphered string.
         """    
         string = self.remove_punctuation(string)  
-        string = re.sub(r'[J]','I',string)
-        if len(string)%2 == 1: string = string + 'X'
+        string = re.sub(r'[J]', 'I', string)
+        if len(string) % 2 == 1:
+            string += 'X'
         ret = ''
-        for c in range(0,len(string),2):
-            ret += self.encipher_pair(string[c],string[c+1])
+        for c in range(0, len(string), 2):
+            ret += self.encipher_pair(string[c], string[c + 1])
         return ret    
 
-    def decipher(self,string):
+    def decipher(self, string):
         """Decipher string using Playfair cipher according to initialised key. Punctuation and whitespace
         are removed from the input. The ciphertext should be an even number of characters. If the input ciphertext is not an even number of characters, an 'X' will be appended.
 
@@ -64,10 +73,11 @@ class Playfair(Cipher):
         :returns: The deciphered string.
         """    
         string = self.remove_punctuation(string)  
-        if len(string)%2 == 1: string = string + 'X'
+        if len(string) % 2 == 1:
+            string += 'X'
         ret = ''
-        for c in range(0,len(string),2):
-            ret += self.decipher_pair(string[c],string[c+1])
+        for c in range(0, len(string), 2):
+            ret += self.decipher_pair(string[c], string[c + 1])
         return ret    
         
 if __name__ == '__main__': 
